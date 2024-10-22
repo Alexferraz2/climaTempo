6d2dbb2919251a7db3797738741a7d21c56da5a0
Alexferraz2 <alex_ferraz2@hotmail.com>
2024-10-22 09:12:18 -0300

    Configurado Styled-component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
-export default function homePage () {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
+export const Home = () => {
+    return (
+        <div>
+            <h1>Hello world!!!</h1>
+        </div>
+    )
 }
+
+export default Home;
